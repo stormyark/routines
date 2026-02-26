f675bb35b6b82d0cbd07559a70b120bab0413c5b
stormyark <stormyark@gmail.com>
2026-02-26 18:02:54 +0100

    First Plugin Idea

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,47 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { App, Modal, Plugin, PluginSettingTab, Setting, TFile, moment, normalizePath, Notice, FileSystemAdapter } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+interface RoutinesSettings {
+	dailyNoteFolder: string;
+}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: RoutinesSettings = {
+	dailyNoteFolder: '/'
+}
+
+export default class RoutinesPlugin extends Plugin {
+	settings: RoutinesSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('sun', 'Start Morning Routine', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new MorningRoutineModal(this.app, this).open();
 		});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+		// Add a class for custom styling
+		ribbonIconEl.addClass('morning-routine-ribbon-class');
 
-		// This adds a simple command that can be triggered anywhere
+		// This adds a command that can be triggered from the Command Palette
 		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
+			id: 'start-morning-routine',
+			name: 'Start Morning Routine',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
+				new MorningRoutineModal(this.app, this).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+		this.addSettingTab(new RoutinesSettingTab(this.app, this));
 	}
 
 	onunload() {
+		// Plugin cleanup
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
@@ -82,18 +49,198 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+export class MorningRoutineModal extends Modal {
+	plugin: RoutinesPlugin;
+	time: string;
+	mood: number = 5;
+	productivity: number = 5;
+
+	constructor(app: App, plugin: RoutinesPlugin) {
 		super(app);
+		this.plugin = plugin;
+		// Auto-fill time with current system time in HH:mm format
+		this.time = moment().format('HH:mm');
 	}
 
 	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.empty();
+		contentEl.addClass('morning-routine-modal');
+
+		contentEl.createEl('h2', { text: 'Morning Routine' });
+
+		// 1. What time is it?
+		new Setting(contentEl)
+			.setName('What time is it?')
+			.addText(text => {
+				text.inputEl.type = 'time';
+				text.setValue(this.time)
+					.onChange(value => {
+						this.time = value;
+					});
+			});
+
+		// 2. How is your mood?
+		new Setting(contentEl)
+			.setName('How is your mood?')
+			.setDesc('1 (Poor) to 10 (Excellent)')
+			.addSlider(slider => slider
+				.setLimits(1, 10, 1)
+				.setValue(this.mood)
+				.setDynamicTooltip()
+				.onChange(value => {
+					this.mood = value;
+				}));
+
+		// 3. How productive are you feeling?
+		new Setting(contentEl)
+			.setName('How productive are you feeling?')
+			.setDesc('1 (Poor) to 10 (Excellent)')
+			.addSlider(slider => slider
+				.setLimits(1, 10, 1)
+				.setValue(this.productivity)
+				.setDynamicTooltip()
+				.onChange(value => {
+					this.productivity = value;
+				}));
+
+		// 4. Submit Button
+		new Setting(contentEl)
+			.addButton(btn => btn
+				.setButtonText('Submit')
+				.setCta()
+				.onClick(async () => {
+					await this.onSubmit();
+					this.close();
+				}));
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
+	}
+
+	async onSubmit() {
+		const now = moment(`1970-01-01T${this.time}`);
+		// Extract hours and minutes from the time input, then apply to current moment
+		const fileDate = moment().set({
+			hour: now.hours(),
+			minute: now.minutes()
+		});
+
+		const formattedDate = fileDate.format('DD.MM.YYYY HH:mm');
+		const displayDate = fileDate.format('dddd, MMMM Do, YYYY');
+		const fileName = fileDate.format('YYYY-MM-DD') + '.md';
+
+		const markdownEntry = `---
+date: ${formattedDate}
+tags:
+  - daily
+cssclasses:
+  - daily
+mood: ${this.mood}/10
+productivity: ${this.productivity}/10
+---
+# DAILY NOTE
+## ${displayDate}
+---
+What i've done today:
+
+
+---
+### Notes created today
+\`\`\`dataview
+List FROM "" WHERE file.cday = date("<%tp.date.now("YYYY-MM-DD")%>") SORT file.ctime asc
+\`\`\`
+
+### Notes last touched today
+\`\`\`dataview
+List FROM "" WHERE file.mday = date("<%tp.date.now("YYYY-MM-DD")%>") SORT file.mtime asc
+\`\`\`
+
+`;
+
+		let folderPath = this.plugin.settings.dailyNoteFolder;
+		if (folderPath === '/' || folderPath === '') {
+			folderPath = '';
+		} else if (!folderPath.endsWith('/')) {
+			folderPath += '/';
+		}
+
+		const filePath = normalizePath(folderPath + fileName);
+		let abstractFile = this.app.vault.getAbstractFileByPath(filePath);
+		let fileToOpen: TFile | null = null;
+
+		if (abstractFile instanceof TFile) {
+			// Do not overwrite, maybe just notify
+			fileToOpen = abstractFile;
+		} else if (!abstractFile) {
+			// Create file if it doesn't exist
+
+			// ensure folder exists
+			if (folderPath !== '') {
+				const folderObj = this.app.vault.getAbstractFileByPath(normalizePath(this.plugin.settings.dailyNoteFolder));
+				if (!folderObj) {
+					await this.app.vault.createFolder(normalizePath(this.plugin.settings.dailyNoteFolder));
+				}
+			}
+
+			fileToOpen = await this.app.vault.create(filePath, markdownEntry);
+		}
+
+		// Open the file
+		if (fileToOpen) {
+			await this.app.workspace.getLeaf('tab').openFile(fileToOpen);
+		}
+
+		// Push to remote using git
+		try {
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			const { exec } = require('child_process');
+			const adapter = this.app.vault.adapter;
+
+			if (adapter instanceof FileSystemAdapter) {
+				const basePath = adapter.getBasePath();
+				new Notice("Pushing vault to remote...");
+
+				exec('git add . && git commit -m "Morning Routine update" && git push', { cwd: basePath }, (error: any, stdout: string, stderr: string) => {
+					if (error) {
+						console.error(`Git push error: ${error.message}`);
+						new Notice("Failed to push to remote. Check console.");
+						return;
+					}
+					new Notice("Successfully pushed to remote.");
+				});
+			}
+		} catch (e) {
+			console.error("Child process error:", e);
+			new Notice("Could not push to remote. Check console.");
+		}
+	}
+}
+
+class RoutinesSettingTab extends PluginSettingTab {
+	plugin: RoutinesPlugin;
+
+	constructor(app: App, plugin: RoutinesPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Daily Note Folder')
+			.setDesc('Folder where daily notes are created (e.g., "Journal" or "Daily Notes"). Leave blank for root directory.')
+			.addText(text => text
+				.setPlaceholder('Example: Journal')
+				.setValue(this.plugin.settings.dailyNoteFolder)
+				.onChange(async (value) => {
+					this.plugin.settings.dailyNoteFolder = value;
+					await this.plugin.saveSettings();
+				}));
 	}
 }
